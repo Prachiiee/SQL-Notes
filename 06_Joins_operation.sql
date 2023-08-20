ec1de8e6e4a5cc500ad51e08c59899878f54e72d
Prachi Jadhav <60337390+Prachiiee@users.noreply.github.com>
2023-08-20 19:42:35 +0530

    Update 06_Joins_operation.sql

--- 06_Joins_operation.sql
+++ 06_Joins_operation.sql
@@ -1,1 +1,47 @@
 -- 1. show the matchid and player name for all goals scored by Germany. To identify German players, check for: teamid = 'GER'
+SELECT matchid , player
+FROM goal
+WHERE teamid = 'GER'
+
+-- 2. Show id, stadium, team1, team2 for just game 1012
+SELECT id,stadium,team1,team2
+FROM game
+WHERE id = 1012
+
+-- 3. show the player, teamid, stadium and mdate for every German goal.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
